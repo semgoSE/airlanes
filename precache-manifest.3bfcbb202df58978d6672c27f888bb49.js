14928acf8b85ed3c7fb8fc226d0527fe044432a7
semgoSE <semgoSE@gmail.com>
2020-07-03 23:31:09 +0300

    Updates

---
+++ precache-manifest.3bfcbb202df58978d6672c27f888bb49.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b4eaef545952f2414b74d8e1360da79",
+    "url": "/airlanes/index.html"
+  },
+  {
+    "revision": "a68eb0296ebe086bbda9",
+    "url": "/airlanes/static/css/2.8c9b10a9.chunk.css"
+  },
+  {
+    "revision": "a68eb0296ebe086bbda9",
+    "url": "/airlanes/static/js/2.3edff615.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/airlanes/static/js/2.3edff615.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b10af65cda71832bee38",
+    "url": "/airlanes/static/js/main.e7a968f7.chunk.js"
+  },
+  {
+    "revision": "1f50f898da9c8b99372a",
+    "url": "/airlanes/static/js/runtime-main.68f54f5d.js"
+  }
+]);

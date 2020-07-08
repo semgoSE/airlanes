3dae13d905bfcd9687899ad125ce2cd54187ad65
semgoSE <semgoSE@gmail.com>
2020-07-08 15:03:46 +0300

    Updates

--- precache-manifest.3ce3e30328558ab72337573ea9eabee5.js
+++ precache-manifest.3ce3e30328558ab72337573ea9eabee5.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "98542911966fd1387cf9e2aff5181eb6",
+    "revision": "c5b99ec1df3c460ab4d8fef48a1159db",
     "url": "/airlanes/index.html"
   },
   {
-    "revision": "f7e8e8e97f3a60c0127b",
+    "revision": "6586b979b005fbf15c73",
     "url": "/airlanes/static/css/2.8c9b10a9.chunk.css"
   },
   {
-    "revision": "f7e8e8e97f3a60c0127b",
-    "url": "/airlanes/static/js/2.15f018d2.chunk.js"
+    "revision": "6586b979b005fbf15c73",
+    "url": "/airlanes/static/js/2.2c5ca76a.chunk.js"
   },
   {
     "revision": "a226718b36494ab10cf4b98c95fd77e3",
-    "url": "/airlanes/static/js/2.15f018d2.chunk.js.LICENSE.txt"
+    "url": "/airlanes/static/js/2.2c5ca76a.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "192b8d03b984b21d65b4",
-    "url": "/airlanes/static/js/main.2b8b5985.chunk.js"
+    "revision": "90371f5872e53709c7aa",
+    "url": "/airlanes/static/js/main.76da14dc.chunk.js"
   },
   {
     "revision": "1f50f898da9c8b99372a",

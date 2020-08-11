bc065d188a32b59ec11a5374fcce2f8c7d33cb3e
semgoSE <semgoSE@gmail.com>
2020-08-11 22:18:20 +0300

    Updates

---
+++ precache-manifest.37072016ca3b180a47b4a875c6023e06.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c97bce11750701a4f109dbd7b9e2f74e",
+    "url": "/airlanes/index.html"
+  },
+  {
+    "revision": "d8f04ac254000ee5a822",
+    "url": "/airlanes/static/css/2.1e562f28.chunk.css"
+  },
+  {
+    "revision": "e285961b7762eda49eaf",
+    "url": "/airlanes/static/css/main.3dd2c4ee.chunk.css"
+  },
+  {
+    "revision": "d8f04ac254000ee5a822",
+    "url": "/airlanes/static/js/2.18c99756.chunk.js"
+  },
+  {
+    "revision": "e285961b7762eda49eaf",
+    "url": "/airlanes/static/js/main.359a3437.chunk.js"
+  },
+  {
+    "revision": "2ea48836d6c8b81bb14b",
+    "url": "/airlanes/static/js/runtime-main.b637fd51.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/airlanes/precache-manifest.c8fdf3d0182607eb3acd89ffed4fc118.js"
+  "/airlanes/precache-manifest.37072016ca3b180a47b4a875c6023e06.js"
 );
 
 self.addEventListener('message', (event) => {
